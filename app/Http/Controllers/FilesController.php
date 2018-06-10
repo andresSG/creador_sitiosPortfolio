b21b2ed8c84c4a209895513afe40929858b4c225
Andres Sanchez Garcia <andresSG@users.noreply.github.com>
2018-06-10 04:09:04 +0200

    Se añaden los ficheros relativos a la plantilla para empresa
    https://colorlib.com/wp/template/glint/
    
    Modificamos este template.
    
    Falta:
    -Controlar excepciones BD
    -Controlar algunas validaciones
    -Lista proyectos admin
    -Terminar generar propiedades y luego  como leerlas en las plantillas

--- app/Http/Controllers/FilesController.php
+++ app/Http/Controllers/FilesController.php
@@ -1,10 +1,40 @@
 <?php
 
 namespace App\Http\Controllers;
+use Illuminate\Support\Facades\DB;
 
 class FilesController extends Controller {
 	//
 	public function generateFile() {
-		return view('home');
+		//obtenemos las rutas
+		$ruteEmpresa = DB::table('master')
+				->where('key', 'empresa_path')->get()->first()->value;
+		$rutePortFolio = DB::table('master')
+				->where('key', 'portfolio_path')->get()->first()->value;
+		$ruteOUT = DB::table('master')
+				->where('key', 'out_path')->get()->first()->value;
+
+		if($empresa){//comprobamos el tipo y obtenemos los ficheros
+			writeProperties($ruteEmpresa);
+			$files = glob(public_path($ruteEmpresa.'/*'));
+		}else{
+			writeProperties($rutePortFolio);
+			$files = glob(public_path($rutePortFolio.'/*'));
+		}
+
+		//escribimos zip
+        \Zipper::make(public_path($ruteOUT.'test.zip'))->add($files)->close();
+
+        return response()->download(public_path($ruteOUT.'test.zip'));//lo enviamos para descargar!!
+	}
+
+	private function writeProperties($ruta){
+		//$fp = fopen("./laravel/andres_t/" . date("Ymd") . $ultimo . ".html", "w+") or die("Unable to open file!");
+		$fp = fopen($ruta."properties.txt", "w+") or die("Unable to open file!");
+		$contenido = 
+		;
+		echo $fp;
+		fwrite($fp, $contenido);
+		fclose($fp);
 	}
 }
